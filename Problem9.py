e714fd8991215078d892c39437fa7069a26aea54
CriticalInfinity <daniyalanis98@gmail.com>
2020-11-25 16:31:36 +0530

    Solved Problem 9

---
+++ Problem9.py
@@ -0,0 +1,23 @@
+'''
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+
+product=0
+for a in range(1,1000//3):
+    for b in range(1,1000//2):
+        c=1000-a-b
+        if c<0:
+            break
+        if a**2+b**2==c**2:
+            if a*b*c>product:
+                product=a*b*c
+            
+
+
+print("The product is ",product)
